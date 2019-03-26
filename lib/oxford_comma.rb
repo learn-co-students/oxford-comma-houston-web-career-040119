babc9aa74eb530c5f7ab5e2d80cfa6bf87f46c7d
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-26 02:00:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    #length = array.length
+    "#{array[0...-1].join(", ")}, and #{array[-1]}"
+  end   
 end
+
+
+puts oxford_comma(["nathan", "iqra", "ted", "paul", "ben"])
